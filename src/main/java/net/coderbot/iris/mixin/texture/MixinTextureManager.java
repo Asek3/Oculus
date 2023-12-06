826d31b5177b6ee55781ec15dba72403363c2c72
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-12-06 14:24:44 -0500

    Fix some parity issues with Forge

--- src/main/java/net/coderbot/iris/mixin/texture/MixinTextureManager.java
+++ src/main/java/net/coderbot/iris/mixin/texture/MixinTextureManager.java
@@ -15,7 +15,7 @@ import java.util.concurrent.Executor;
 
 @Mixin(TextureManager.class)
 public class MixinTextureManager {
-	@Inject(method = "m_244739_", at = @At("TAIL"))
+	@Inject(method = "method_18167", at = @At("TAIL"))
 	private void iris$onTailReloadLambda(ResourceManager resourceManager, Executor applyExecutor, CompletableFuture<?> future, Void void1, CallbackInfo ci) {
 		TextureFormatLoader.reload(resourceManager);
 		PBRTextureManager.INSTANCE.clear();
