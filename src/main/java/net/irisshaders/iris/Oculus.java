7a3248878e6f411a3fd86244c01afd4457819cf1
Julian <julian@aternos.org>
2024-05-11 17:02:29 +0200

    Prevent crashing on servers

---
+++ src/main/java/net/irisshaders/iris/Oculus.java
@@ -0,0 +1,15 @@
+package net.irisshaders.iris;
+
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
+
+@Mod(Oculus.MODID)
+public class Oculus {
+    public static final String MODID = "oculus";
+
+    public Oculus() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
+}
